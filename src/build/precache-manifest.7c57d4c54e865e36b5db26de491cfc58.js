a86e03111d4ce3096ff94de5d4f2dcc1f22a57cd
czpta <czapata96.cz@gmail.com>
2019-04-30 09:10:46 -0400

    Upload build folder

---
+++ src/build/precache-manifest.7c57d4c54e865e36b5db26de491cfc58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4c21676de269d22b6fd0",
+    "url": "/static/js/main.19965e74.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "4c21676de269d22b6fd0",
+    "url": "/static/css/main.fea5b02d.chunk.css"
+  },
+  {
+    "revision": "018e11934f41c602ec76f514a245f62c",
+    "url": "/index.html"
+  }
+];
